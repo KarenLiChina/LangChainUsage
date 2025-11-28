abcb18b70cbaa5edc01263e947ceb7f989490167
KarenLi <chinalilikun@126.com>
2025-11-28 10:15:20 +0800

    call openai with langchain, add langsmith configuration

---
+++ call_openai.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+# 调用LLM
+# 1. 创建模型
+
+model = ChatOpenAI(model='gpt-4-turbo',
+                   api_key=os.getenv("OPENAI_API_KEY"),
+                   base_url=os.getenv("BASE_URL"))
+# 2. 准备提示词 prompt
+msg = [
+    SystemMessage(content='请将一下的内容翻译为英语'),
+    HumanMessage(content="明月几时有，把酒问青天。")
+]
+
+
+# 3. 创建返回数据的解析器
+parser = StrOutputParser()  # langchain中最简单的解析工具
+
+# 4. 得到链
+chain = model | parser
+
+# 5. 直接使用chain调用
+print(chain.invoke(msg)) #
+
+# 链式调用等价与下面的代码
+# result = model.invoke(msg)
+# print(parser.invoke(result)) # 解析相应之后的结果
--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 ## 配置环境变量
 
 创建 `.env`文件，在env文件中的设置 `OPENAI_API_KEY` 和 `BASE_URL` 为自己的 key 和 url
+`LANGCHAIN_TRACING_V2`设置为true，`LANGCHAIN_API_KEY`设置为LangSmith的API Key，可以在LangSmith中查看调用大模型使用情况，不需要也可以不配置这两个变量
+
+# langsmith的检测数据
+配置`LANGCHAIN_TRACING_V2`，`LANGCHAIN_API_KEY`后可以在https://smith.langchain.com/ Tracing Projects中查看调用大模型的使用情况
